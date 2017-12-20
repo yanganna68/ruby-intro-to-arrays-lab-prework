9d7693af4f0271c6e407ebda1602a7a0f0c104fe
ANNA YANG <yanganna68@gmail.com>
2017-12-20 20:42:20 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,19 @@
+def instantiate_new_array
+  Array.new
+end
+
+def array_with_two_elements
+  return [2,3]
+end
+
+def first_element(arr)
+  arr.first
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  arr.last
+end
